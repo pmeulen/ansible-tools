b1b2fd5e48ef744a591cffdfea195da65fbd9721
Pieter van der Meulen <pieter.vandermeulen@surfnet.nl>
2018-12-18 10:46:45 +0100

    Run tests from python3 test environment too

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -6,6 +6,7 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 bats_dir=${script_dir}/bats
 BATS=${bats_dir}/bin/bats
+py3_dir=${script_dir}/py3
 
 CWD=`pwd`
 
@@ -22,6 +23,10 @@ if [ ! -f  ${BATS} ]; then
     cd $CWD
 fi
 
+echo "Running tests using distro python and ansible version:"
+python --version
+ansible --version
+
 cd ${script_dir}
 args=$@
 if [ -z "$args" ]; then
@@ -30,5 +35,37 @@ fi
 echo "Running ${BATS} $args"
 ${BATS} $args
 res=$?
+if [ ! $res -eq 0 ]; then
+    echo "Python 2 tests failed"
+    cd $CWD
+    exit $res
+fi
+
+if [ ! -d ${py3_dir} ]; then
+    echo "Creating python3 environment for running tests"
+    cd ${script_dir}
+    pyvenv py3
+    source py3/bin/activate
+    pip install --upgrade pip
+    pip install ansible
+    pip install python3-keyczar
+fi
+
+echo "Running tests using ansible and keyczart from python3 environment:"
+cd ${script_dir}
+source py3/bin/activate
+python --version
+ansible --version
+
+echo "Running ${BATS} $args"
+${BATS} $args
+res=$?
+
+if [ ! $res -eq 0 ]; then
+    echo "Python 3 tests failed"
+    cd $CWD
+    exit $res
+fi
+
 cd $CWD
 exit $res
